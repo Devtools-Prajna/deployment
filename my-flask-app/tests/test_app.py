9bd883f4904c0e64f17bb331902c017599e324b0
Vinod-DevTools <vinodm@devtools.in>
2025-07-02 12:40:47 +0530

    Update test_app.py

--- my-flask-app/tests/test_app.py
+++ my-flask-app/tests/test_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add my-flask-app directory to the sys.path so Python can find app.py
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 def test_home():
